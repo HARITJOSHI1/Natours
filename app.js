99825df10b391b35cbf8895f7e509c28b0ea78c0
Harit Joshi <joshiharit3@gmail.com>
2021-05-29 18:21:54 +0530

    minor updates

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ if(process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
 app.use(express.json());
 
 // Mounting the router
-app.use('/api/v1/tour', tourRoute);
+app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
+
+// Handling unknown routes
+app.all('*', function(req, res, next) {
+    res.status(404).json({
+        status: 'failed',
+        message: `Cannot find ${req.originalUrl} on this server`
+    });
+
+    next();
+});
 
 // Exporting app
 module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,76 +1,11 @@
 const express = require('express');
-const tours = require('./../models/tourModel');
-
-// const dataJson = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'UTF-8')
-// );
 
 // FEATURES OF OUR API Class:
+const APIfeatures = require('./APIFeatureClass');
 
-class APIfeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
+const tours = require('./../models/tourModel');
 
-  // 1. Filtering
-  filter = () => {
-    const queryObj = { ...this.queryString }; // shallow copy
-    const excludeParams = ['page', 'sort', 'limit', 'fields'];
-    excludeParams.forEach((ptr) => delete queryObj[ptr]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
-    queryStr = JSON.parse(queryStr);
-
-    // To chain multiple functions on the query obj later
-    this.query = this.query.find(queryStr);
-    return this;
-  };
-
-  // 2. Sorting
-  sort = () => {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-      return this;
-      // query = query.sort('(field name) (other field name ...)')
-    } else {
-      this.query = this.query.sort('-createdAt');
-      return this;
-    }
-  };
-
-  // 3. Limiting fields (projection)
-  fields = () => {
-    if (this.queryString.fields) {
-      const fieldBy = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fieldBy);
-      return this;
-    } else {
-      this.query = this.query.select('-__v'); // To exclude a field
-      return this;
-    }
-  };
-
-  // 4. Pagination
-  page = (total) => {
-    const page = +this.queryString.page || 1;
-    const limit = +this.queryString.limit || 10;
-    const skip = (page - 1) * limit;
-    this.query = this.query.skip(skip).limit(limit);
-
-    if (!this.queryString.limit) {
-      this.query = this.query.limit(limit);
-    }
-
-    if (total <= skip) {
-      throw new Error('Page out not found');
-    }
-    return this;
-  };
-}
-
+  
 // #################################################################
 
 // FUNCTIONS
