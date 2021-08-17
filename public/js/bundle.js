238f21dcf25651e35096c3fa9a9dc26c8e5fced7
Harit Joshi <joshiharit3@gmail.com>
2021-08-17 21:48:27 +0530

    error rendering

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8918,7 +8918,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45761" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33345" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- utils/Errors.js
+++ utils/Errors.js
@@ -3,47 +3,79 @@ const AppError = require('./AppError');
 // Database related errors
 const handleCastErrorDB = (e) => {
   return new AppError(`Invalid id with value ${e.value}`, 400);
-}
+};
 
 const handleValidatorErrorDB = (e) => {
   return new AppError(`${e.errors.name.value} field is ambigous`, 400);
-}
+};
 
 const handleDuplicateErrorDB = (e) => {
-  return new AppError(`${e.errors.name.properties.path} field is required`, 400);
-}
+  return new AppError(
+    `${e.errors.name.properties.path} field is required`,
+    400
+  );
+};
 
 const handleInvalidJsonWebToken = (e) => {
   return new AppError(e.message, 401);
-}
- 
+};
+
 const handleTokenExpire = (e) => {
-  return new AppError("Your token has already expired! Please login again", 401);
-}
+  return new AppError(
+    'Your token has already expired! Please login again',
+    401
+  );
+};
 
+const setDevErr = (err, req, res) => {
+  //  A) API
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
 
-const setDevErr = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+  // B) Rendered website
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong !',
+    msg: err.message,
   });
 };
 
-const setProdErr = (err, res) => {
-  if (!err.isOperational) {
-    console.error('Error ⛔', err);
-    res.status(500).json({
-      status: 'failed',
+const setProdErr = (err, req, res) => {
+  
+  //  A) API
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(500).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+
+    return res.status(err.statusCode).json({
+      status: 'error',
       message: 'Something went wrong !',
     });
-  } else {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err.message,
+  }
+
+  //  B) Rendered website
+  if (err.isOperational) {
+    console.log(err);
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message,
     });
   }
+
+  console.log(err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later.',
+  });
 };
 
 // Handle all errors
@@ -52,32 +84,32 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    setDevErr(err, res);
-  } 
-  else {
-    let error = {... err};
+    setDevErr(err, req, res);
+  } else {
+    let error = { ...err };
+    error.message = err.message
 
-    if(error.kind === 'ObjectId'){
+    if (error.kind === 'ObjectId') {
       error = handleCastErrorDB(error);
     }
 
-    if(error._message === 'Tour validation failed'){
+    if (error._message === 'Tour validation failed') {
       error = handleValidatorErrorDB(error);
     }
 
-    if(error.kind === 'User defined'){
+    if (error.kind === 'User defined') {
       error = handleDuplicateErrorDB(error);
     }
-    
-    if(error.name === 'JsonWebTokenError'){
+
+    if (error.name === 'JsonWebTokenError') {
       error = handleInvalidJsonWebToken(error);
     }
 
-    if(error.name === 'TokenExpiredError'){
+    if (error.name === 'TokenExpiredError') {
       error = handleTokenExpire();
     }
 
-    setProdErr(error, res);
+    setProdErr(error, req, res);
   }
   next();
 };
